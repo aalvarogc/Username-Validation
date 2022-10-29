21f0361426fd31acae3bc568b7c36f300b5e6e1c
Álvaro García <alvaropulu03@gmail.com>
2022-10-29 18:49:01 +0200

    Uploading the exercise

---
+++ index.js
@@ -0,0 +1,32 @@
+validations = document.getElementsByTagName("li");
+
+oneLowercase = new RegExp(".*[a-z].*");
+oneUppercase = new RegExp(".*[A-Z].*");
+minimunChars = new RegExp("^.{6,}$")
+
+function checkUsername(e){
+    if(oneLowercase.test(e.value)){
+        validations[0].style.color = "green";
+        console.log("si")
+    }else{
+        validations[0].style.color = "white";
+        console.log("no")
+    }
+
+    if(oneUppercase.test(e.value)){
+        validations[1].style.color = "green";
+    }else{
+        validations[1].style.color = "white";
+    }
+    
+    if(minimunChars.test(e.value)){
+        validations[2].style.color = "green";
+    }else{
+        validations[2].style.color = "white";
+    }
+}
+
+window.onload = ()=>{
+    username = document.getElementById("username");
+    username.addEventListener("keyup", ()=>{checkUsername(username)});
+}
